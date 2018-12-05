44450fdcb65ce3bc2b09eb6b64c11e3e015626fb
Nguyen Hai Duy <nguyenhaiduy2894@gmail.com>
2018-12-06 00:07:39 +0700

    init

---
+++ src/main/java/com/coexplore/admin/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.coexplore.admin.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+    @RequestMapping("/")
+    public String home(Model model) {
+        return "index";
+    }
+    
+    @RequestMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+}
